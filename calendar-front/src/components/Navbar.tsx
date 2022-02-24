de458d4904c9450790bf1c67885af784927198e2
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-02-23 21:48:04 -0500

    Navigation bar events

--- calendar-front/src/components/Navbar.tsx
+++ calendar-front/src/components/Navbar.tsx
@@ -6,11 +6,26 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { palette } from '../lib/style';
+import { Home, Logout } from '@mui/icons-material';
+import { PATH } from '../lib/consts';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const onClick = (path: string) => {
+        navigate(path);
+    }
+
+    const onLogout = () => {
+        navigate(`/${PATH.login}`);
+    }
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
+        <Box sx={{flexGrow: 1, backgroundColor: palette.primary }}>
+            <AppBar position="static" style={{ position: 'relative' }}>
                 <Toolbar>
                     <IconButton
                         size="large"
@@ -18,14 +33,31 @@ function Navbar() {
                         color="inherit"
                         aria-label="menu"
                         sx={{ mr: 2 }}
+                        onClick={() => onClick(`/${PATH.root}/${PATH.home}`)}
                     >
-                        <MenuIcon />
+                        <Home />
                     </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit" onClick={() => onClick(`/${PATH.root}/${PATH.calendar}`)}>Administrar Calendario</Button>
                 </Toolbar>
+                <div style={{
+                    position: "absolute",
+                    right: "0",
+                    height: "100%",
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        onClick={onLogout}
+                    >
+                        <Logout />
+                    </IconButton>
+                </div>
             </AppBar>
         </Box>
     )
--- calendar-front/src/pages/Main.tsx
+++ calendar-front/src/pages/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 import { PATH } from '../lib/consts';
 import Calendar from './subpages/Calendar';
 import Welcome from './subpages/Welcome';
@@ -8,11 +9,14 @@ import Welcome from './subpages/Welcome';
 function Main() {
   return (
     <>
-      <Routes>
-        <Route path={`${PATH.home}`} element={<Welcome />} />
-        <Route path={`${PATH.calendar}`} element={<Calendar />} />
-        <Route path="/*" element={<Navigate to={`${PATH.home}`} />} />
-      </Routes>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path={`${PATH.home}`} element={<Welcome />} />
+          <Route path={`${PATH.calendar}`} element={<Calendar />} />
+          <Route path="/*" element={<Navigate to={`${PATH.home}`} />} />
+        </Routes>
+      </div>
     </>
   )
 }
