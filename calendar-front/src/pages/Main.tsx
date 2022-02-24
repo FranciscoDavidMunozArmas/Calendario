feecb6cdcbd809d390c020ce10217a4e0f454455
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-02-23 19:38:52 -0500

    Routes implementation

--- calendar-front/src/pages/Main.tsx
+++ calendar-front/src/pages/Main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Calendar from './subpages/Calendar';
+import Welcome from './subpages/Welcome';
 
 function Main() {
   return (
     <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Welcome />} />
+        <Route path="/calendar" element={<Calendar />} />
+      </Routes>
     </BrowserRouter>
   )
 }
