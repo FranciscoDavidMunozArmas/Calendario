aeafc89bca2da3fa5823c1250666a5f3db1a58bd
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-02-23 20:35:08 -0500

    Styles

---
+++ calendar-front/src/lib/style.ts
@@ -0,0 +1,10 @@
+export const styles = {
+    primary: '#338CFF',
+    containerFlex: {
+        display: 'flex',
+        flexDirection: 'column' as const,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+    },
+}
