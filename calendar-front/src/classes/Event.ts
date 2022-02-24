e6304f6c66028298e21c1237739df0e82e482dfe
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-02-23 22:42:49 -0500

    Event Class

---
+++ calendar-front/src/classes/Event.ts
@@ -0,0 +1,32 @@
+export class Event {
+    id: number;
+    title: string;
+    startDate: Date;
+    endDate: Date;
+
+    constructor(id: number, title: string, startDate: Date, endDate: Date) {
+        this.id = id;
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
+
+export const EventConverter = {
+    fromJSON: (json: any): Event => {
+        return new Event(
+            json.id,
+            json.title,
+            new Date(json.startDate),
+            new Date(json.endDate)
+        );
+    },
+    toJSON: (event: Event): any => {
+        return {
+            id: event.id,
+            title: event.title,
+            startDate: event.startDate.toISOString(),
+            endDate: event.endDate.toISOString()
+        };
+    }
+};
