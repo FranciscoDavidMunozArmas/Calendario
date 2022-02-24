c7dba0eba669f3447dbbe6ba593ee9478ae49d58
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-02-23 19:34:17 -0500

    React typescript

--- calendar-front/src/pages/Main.tsx
+++ calendar-front/src/pages/Main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function Main() {
   return (
-    <div>Main</div>
+    <BrowserRouter>
+    </BrowserRouter>
   )
 }
 
