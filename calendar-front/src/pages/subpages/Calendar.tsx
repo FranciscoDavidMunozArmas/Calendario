13242f9948a725e732a8b1b99bc2ed6197df4d19
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-02-25 18:22:03 -0500

    Fix calendar height and headerToolbar

--- calendar-front/src/pages/subpages/Calendar.tsx
+++ calendar-front/src/pages/subpages/Calendar.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from "@fullcalendar/interaction"
 import { Backdrop, Fade, Modal } from '@mui/material';
 import { Box } from '@mui/system';
@@ -133,11 +134,20 @@ function Calendar() {
   return (
     <>
       <Loading load={loading} />
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        events={formatEvents()}
-        eventClick={handleEventClick}
-      />
+      <div style={{ margin: '10px 0' }}>
+        <FullCalendar
+          initialView="dayGridMonth"
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,dayGridWeek,dayGridDay',
+          }}
+          height={window.innerHeight * .9}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          events={formatEvents()}
+          eventClick={handleEventClick}
+        />
+      </div>
 
       <button style={styles.floatingButton} onClick={handleOpen}><Add /></button>
 
